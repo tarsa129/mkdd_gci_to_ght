541dd8f3dcd1990ff545b9eb08a9e770ae6d7161
tarsa129 <sheehant1293@gmail.com>
2020-08-14 02:44:11 -0400

    Update gci2ght.py

--- gci2ght.py
+++ gci2ght.py
@@ -2,7 +2,7 @@ def get_course_name(course_no):
     course_list = ["BabyLuigi", "Peach", "Daisy", "Luigi", "Mario", "Yoshi", 
     "Nokonoko", "Patapata", "Waluigi", "Wario", "Diddy", "Donkey", "Koopa", 
     "you shouldn't get this", "Rainbow", "Desert", "Snow"]
-    return course_list[course_no - 35]
+    return course_list[course_no - 33]
 
 
 
@@ -18,8 +18,10 @@ if __name__ == "__main__":
     
     with open(args.input, "rb") as f:
         f.seek(5251)
-        course_id = f.read(1); 
+        course_id = f.read(1)
+        print(course_id)
         course_id = ord(course_id)
+        
         output = get_course_name(course_id) + ".ght"
         print(output)
         with open(output, "wb") as o:
